22662a60cafb7f005e2960b45d0065e241502c34
blue-putty <65861156+blue-putty@users.noreply.github.com>
2021-11-18 21:31:23 -0500

    reorganize structure

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,17 @@
-const path = require('path');
-const BookmarkletPlugin = require('./webpack/BookmarkletPlugin');
+const path = require("path");
+const BookmarkletPlugin = require("./webpack/BookmarkletPlugin");
 
 module.exports = {
-    mode: 'production',
-    entry: {
-        comment: './src/comment.js',
-        focusMode: './src/focusMode.js',
-        forwardAudio: './src/forwardAudio.js',
-        rewindAudio: './src/rewindAudio.js',
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].bundle.js',
-    },
-    plugins: [new BookmarkletPlugin()]
+  mode: "production",
+  entry: {
+    comment: "./src/comment.js",
+    focusMode: "./src/focusMode.js",
+    forwardAudio: "./src/audio-utilities/forwardAudio.js",
+    windows: "./src/audio-utilities/rewindAudio.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js",
+  },
+  plugins: [new BookmarkletPlugin()],
 };
