d6239a10ec0658339a08df2984e53c42c816df09
Alexander Yang <65861156+blueputty01@users.noreply.github.com>
2024-01-20 14:47:56 -0500

    add anki

--- src/anki.ts
+++ src/anki.ts
@@ -1,7 +1,24 @@
-function getSelectionText(): string {
-    const text = window.getSelection().toString();
-
-    return text;
+function init(): void {
+    document.addEventListener('keydown', keyHandler);
 }
 
-getSelectionText();
+function keyHandler(event: KeyboardEvent) {
+    if (event.ctrlKey && event.shiftKey && event.code == 'KeyC') {
+        cloze(event);
+        event.preventDefault();
+    }
+}
+
+function cloze(event: KeyboardEvent) {
+    const selection = window.getSelection();
+    const ele = (event.target as Element).childNodes[0];
+
+    console.log(event);
+    console.log(selection);
+
+    const text = ele.textContent;
+
+    ele.textContent = `${text.substring(0, selection.anchorOffset)}{{c1::`;
+}
+
+init();
